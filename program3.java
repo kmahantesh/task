81cabed4e76fce794b6bdd9f2726f97ad6b090fe
mahantesh kumbar <103052636+kmahantesh@users.noreply.github.com>
2022-04-07 19:11:13 +0530

    create program3.java

---
+++ program3.java
@@ -0,0 +1,31 @@
+import java.util.stream.LongStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWorld{
+
+    public static void main(String args[]) throws Exception {
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          String line = br.readLine();
+          int N = Integer.parseInt(line);
+          ArrayList<Integer> inputs = new ArrayList<Integer>();
+          for (int i = 0; i < N; i++) {
+              inputs.add(Integer.valueOf(br.readLine()));
+          }
+          ArrayList<BigInteger> outputs = new ArrayList<BigInteger>();
+          for(Integer input : inputs){
+              outputs.add(factorial(input));
+          }
+          for(BigInteger result: outputs){
+              System.out.println(result);
+          }
+     }
+
+     private static BigInteger factorial(Integer input) {
+          if(input == 1) return BigInteger.ONE;
+          return factorial(input - 1).multiply(new BigInteger(String.valueOf(input)));
+     }
+}
